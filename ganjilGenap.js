bb7f770989913af200f7bce5b19f270a8be0196a
Nur Imam Iskandar <nurimamiskandar@gmail.com>
2023-02-02 10:32:42 +0700

    logic memisahkan angka ganjil dan genap

---
+++ ganjilGenap.js
@@ -0,0 +1,16 @@
+// memisahkan ganjil genap 1-100
+
+var b = 100; // banyaknya bilangan
+var genap = [];
+var ganjil = [];
+
+for (let i = 0; i <= b; i++) {
+  if (i % 2 == 0) {
+    // jika i dibagi 2 = 0 (bilangan genap)
+    genap = [...genap, i];
+  } else {
+    // jikan i dibagi 2 != 0 (bilangan ganjil)
+    ganjil = [...ganjil, i];
+  }
+  console.log({ ganjil, genap });
+}
